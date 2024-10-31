f8f912083b87cc5dbc910dcccdbb5ce3470091ef
Poleexpr <a.c4erkasova@gmail.com>
2024-10-31 17:49:12 +0300

    feat: solve a new one

--- problem.py
+++ problem.py
@@ -1,11 +1,27 @@
-def broke_chocolate(n, m, countN=0, countM=0):
-  if(n == 1 and m == 1):
-    return countN + (countM * (countN + 1))
-  elif(n == 1 and m > 1):
-    return broke_chocolate(n, m-1, countN, countM+1)
-  else:
-    return broke_chocolate(n-1, m, countN+1, countM)
+from datetime import datetime
 
-print(broke_chocolate(1, 1))
+def get_date(date_string):
+  try:
+    date_the_moscow_times = datetime.strptime( date_string, '%A, %B %d, %Y' )
+    return date_the_moscow_times
+  except: 
+    pass
+
+  try:
+    date_the_guardian = datetime.strptime( date_string, '%A, %m.%d.%y' )
+    return date_the_guardian
+  except: 
+    pass
+
+  try:
+    date_daily_news = datetime.strptime( date_string, '%A, %d %B %Y' )
+    return date_daily_news
+  except: 
+    pass
+  
+
+print(get_date('Wednesday, October 2, 2002'))
+print(get_date('Friday, 11.10.13'))
+print(get_date('Thursday, 18 August 1977'))
 
 
