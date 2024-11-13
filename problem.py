f2122f1a84f6a7207322225751c5fd382fe76c77
Poleexpr <a.c4erkasova@gmail.com>
2024-11-13 17:01:06 +0300

    feat: create a htmltojson converter

--- problem.py
+++ problem.py
@@ -1,27 +1,10 @@
-from datetime import datetime
+import json
+import xmltodict
 
-def get_date(date_string):
-  try:
-    date_the_moscow_times = datetime.strptime( date_string, '%A, %B %d, %Y' )
-    return date_the_moscow_times
-  except: 
-    pass
+with open("cards.xml") as xml_file:
+    data_dict = xmltodict.parse(xml_file.read())
 
-  try:
-    date_the_guardian = datetime.strptime( date_string, '%A, %m.%d.%y' )
-    return date_the_guardian
-  except: 
-    pass
+    json_data = json.dumps(data_dict, ensure_ascii=False)
 
-  try:
-    date_daily_news = datetime.strptime( date_string, '%A, %d %B %Y' )
-    return date_daily_news
-  except: 
-    pass
-  
-
-print(get_date('Wednesday, October 2, 2002'))
-print(get_date('Friday, 11.10.13'))
-print(get_date('Thursday, 18 August 1977'))
-
-
+    with open("cards.json", "w") as json_file:
+        json_file.write(json_data)
