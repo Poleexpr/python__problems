c21537a7712307159e72736d9909c9fccf09067a
Poleexpr <a.c4erkasova@gmail.com>
2024-11-20 21:36:39 +0300

    feat: solve a new one

--- problem.py
+++ problem.py
@@ -1,10 +1,47 @@
-import json
-import xmltodict
+from time import perf_counter
+import threading
 
-with open("cards.xml") as xml_file:
-    data_dict = xmltodict.parse(xml_file.read())
+def f(x):
+    return pow(x, 2) - pow(x, 2)+ (x * 4) - (x * 5) + x + x
 
-    json_data = json.dumps(data_dict, ensure_ascii=False)
+def f2(x):
+    return x + x
 
-    with open("cards.json", "w") as json_file:
-        json_file.write(json_data)
+def f3(x):
+    return f(x) + f2(x)
+
+threads = []
+for n in range(0, 10000):
+    t = threading.Thread(target=f, args = {n})
+    t2 = threading.Thread(target=f2, args = {n})
+    threads.append(t)
+    threads.append(t2)
+    t.start()
+    t2.start()       
+
+for t in threads:
+    t.join()
+
+end_time = perf_counter()
+
+print(f'Выполнение заняло {end_time} секунд.')
+
+
+threads3 = []
+for n3 in range(0, 10000):
+    t3 = threading.Thread(target=f3, args = {n})
+    threads.append(t3)
+    t3.start()
+        
+
+for t3 in threads3:
+    t3.join()
+
+end_time3 = perf_counter()
+
+print(f'Выполнение заняло {end_time3} секунд.')
+
+
+
+
+
