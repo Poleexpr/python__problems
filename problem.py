5643c71a6b2fd6465b3f56013782f0d844c65afc
Poleexpr <a.c4erkasova@gmail.com>
2024-12-02 02:29:47 +0300

    feat: solve a new one

--- problem.py
+++ problem.py
@@ -1,21 +1,41 @@
-def findSmallest (arr):
-    smallest = arr[0]
-    smallest_index = 0  
-    for i in range(1, len(arr)):
-        if arr[i] < smallest:
-            smallest = arr[i]
-            smallest_index = i
-    return smallest_index
+def sum(arr):
+    if(len(arr) == 0):
+        return 0
+    elif(len(arr) == 1):
+        return arr[0]
+    else:
+        return arr[0] + sum(arr[1:])
 
-# функция сортировки выбором 
-def selectionSort (arr):
-    newArr = []  
-    for i in range(len(arr)):
-        smallest = findSmallest(arr)
-        newArr.append(arr.pop(smallest))
-    return newArr
 
-print(selectionSort([5, 3, 6, 2, 10]))
+
+def length_arr(arr, k):
+    if(len(arr) == 0):
+        return k
+    else: 
+        return length_arr(arr[1:], k + 1)
+
+
+m = 0
+
+def max(arr):
+    if(type(arr) == int):
+        return arr
+    else:
+        if(arr[0] > arr[1]):
+            return max(arr.pop(0))
+        else:
+            return max(arr.pop(1))
+
+print(max([1, 2, 3, 4]))
+
+def max2(list):
+   if len(list) == 2:
+    return list[0] if list[0] > list[1] else list[1]
+   sub_max = max(list[1:])
+   return list[0] if list[0] > sub_max else sub_max
+
+
+
   
 
 
