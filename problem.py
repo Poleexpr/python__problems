ab4853a22e221b097eb08cab997ceb84a7cce7cd
Poleexpr <a.c4erkasova@gmail.com>
2024-12-11 14:46:45 +0300

    feat: solve a new one

--- problem.py
+++ problem.py
@@ -1,38 +1,15 @@
-def sum(arr):
-    if(len(arr) == 0):
-        return 0
-    elif(len(arr) == 1):
-        return arr[0]
-    else:
-        return arr[0] + sum(arr[1:])
+import re
 
+text = 'A222BC96'
 
+pattern = r'([A-Z]{1})(\d{3})([A-Z]{2})(\d{2,})'
+result = re.findall(pattern, text)
 
-def length_arr(arr, k):
-    if(len(arr) == 0):
-        return k
-    else: 
-        return length_arr(arr[1:], k + 1)
-
-
-m = 0
-
-def max(arr):
-    if(type(arr) == int):
-        return arr
-    else:
-        if(arr[0] > arr[1]):
-            return max(arr.pop(0))
-        else:
-            return max(arr.pop(1))
-
-print(max([1, 2, 3, 4]))
-
-def max2(list):
-   if len(list) == 2:
-    return list[0] if list[0] > list[1] else list[1]
-   sub_max = max(list[1:])
-   return list[0] if list[0] > sub_max else sub_max
+if(len(result) > 0):
+  message = f"Номер {result[0][0]}{result[0][1]}{result[0][2]} валиден. Регион: {result[0][3]}."
+  print(message)
+else:
+  print('Номер не валиден')
 
 
 
